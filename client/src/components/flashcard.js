c2af2079b518d4ac1ec87c57d80569818df77fd2
Kara Mui <kara.mui@dartmouth.edu>
2018-04-26 20:35:22 -0500

    functional, memory issues

--- client/src/components/flashcard.js
+++ client/src/components/flashcard.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import classnames from 'classnames';
 
 const Flashcard = (props) => {
