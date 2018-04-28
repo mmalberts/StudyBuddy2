4900faf4e520690db30582f7dce1b431570b1bd6
Kara Mui <kara.mui@dartmouth.edu>
2018-04-28 11:16:29 -0500

    split components

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles/style.css';
-import './styles/components/Header.css';
-import './styles/components/UserLogin.css';
-import './styles/components/Navbar.css';
-import './styles/components/flashcards.css';
-import './styles/components/About.css';
-import './styles/components/Add.css';
-import './styles/components/questions.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('app')); 
+ReactDOM.render(<App />, document.getElementById("app")); 
---
+++ client/src/pages/SetPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+
+export default class SetPage extends React.Component{
+
+    render() {
+        return (
+            <div className="background example-page">
+	            <Navbar firstName = {localStorage.getItem("user")}/>
+                <About />
+            </div>
+        )
+    }
+};
