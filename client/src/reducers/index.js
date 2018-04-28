04064647dd0ec84188eb98b2ce8662a9211caaf8
Christopher Banaszak <ludachris@dhcp-10-105-83-54.wireless.northwestern.private>
2018-04-28 13:10:29 -0500

    added redux

---
+++ client/src/reducers/index.js
@@ -0,0 +1,14 @@
+
+
+const rootReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_USER':
+      return [ ...state, {firstName: action.user.firstName, id: action.user.id} ];
+
+       
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
