b26f8e15ced9573473ffce708c5c47a549386b5e
Christopher Banaszak <ludachris@dhcp-10-105-83-54.wireless.northwestern.private>
2018-04-28 14:09:50 -0500

    working version with user login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var path = require("path");
 var bodyParser = require("body-parser");
 var express = require("express");
+
 var publicPath = path.join(__dirname, "client/public");
 
 // initializes Express.js server and defines port
@@ -21,12 +22,13 @@ app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"))
 })
+//app.use(routes)
 
 // imports routes
 require("./routes/user-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-// starts Express.js server
+//starts Express.js server
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
