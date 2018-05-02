2747c0ec91c4eccc573b4a5e5bf30c5eaff02461
Christopher Banaszak <ludachris@dhcp-10-105-83-54.wireless.northwestern.private>
2018-05-01 20:29:49 -0500

    added state to the setapge component

--- client/src/pages/SetPage/SetPage.js
+++ client/src/pages/SetPage/SetPage.js
@@ -1,15 +1,25 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import About from "../About";
+import {connect} from "react-redux";
 
-export default class SetPage extends React.Component{
+const mapStateToProps = state => {
+	return {user: state[0]};
+} 
+
+class ConnectedSetPage extends React.Component{
 
     render() {
         return (
             <div className="background example-page">
-	            <Navbar firstName = {localStorage.getItem("user")}/>
+	            <Navbar firstName = {this.props.user.firstName}/>
                 <About />
             </div>
         )
     }
 };
+
+const SetPage = connect(mapStateToProps)(ConnectedSetPage);
+
+export default SetPage;
+
