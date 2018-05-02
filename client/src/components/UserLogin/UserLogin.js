a77cc796c67b3333cd351389d5fb3f28fc066965
Kara Mui <kara.mui@dartmouth.edu>
2018-05-01 20:54:45 -0500

    styled modal

--- client/src/components/UserLogin/UserLogin.js
+++ client/src/components/UserLogin/UserLogin.js
@@ -96,8 +96,8 @@ class Signup extends React.Component {
 
     console.log(data);
     this.setState(() => ({
-      confirmSignup: true
-    }));
+          confirmSignup: true
+        }));
 
     fetch("/api/users", {
       method: "POST",
@@ -116,7 +116,7 @@ class Signup extends React.Component {
           }));
           throw new Error("Bad response from server");
         }
-
+        
         return response.json();
       })
       .catch(function(err) {
