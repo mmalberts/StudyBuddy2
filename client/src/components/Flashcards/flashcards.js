8c243360ab9b5797703cc7b7f861f35576ae75dd
Kara Mui <kara.mui@dartmouth.edu>
2018-05-03 21:33:24 -0500

    added preliminary filter functionality

--- client/src/components/Flashcards/flashcards.js
+++ client/src/components/Flashcards/flashcards.js
@@ -18,6 +18,25 @@ const Flashcards = (props) => {
                 <AddFlashCard 
                     /*handleAddFlashcard = {this.handleAddFlashcard}*/
                     />
+
+                <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cardbox">
+                  <div className="math card card-default">
+                    <button className="white-button card-title">math</button>
+                  </div>
+                </div>
+
+                <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cardbox">
+                  <div className="science card card-default">
+                    <button className="white-button card-title">science</button>
+                  </div>
+                </div>
+
+                <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cardbox">
+                  <div className="english card card-default">
+                    <button className="white-button card-title">english</button>
+                  </div>
+                </div>
+
                 {props.flashcards.map((card) => (
                     <Flashcard 
                     key = {card.unitName}
