517c97b83763dc592ba972ccdc03b58c174845a6
Kara Mui <kara.mui@dartmouth.edu>
2018-05-05 14:10:09 -0500

    adjusted form

--- client/src/pages/Add/Add.js
+++ client/src/pages/Add/Add.js
@@ -77,19 +77,18 @@ class ConnectedQuestionPage extends React.Component {
 	            <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
 	                      <h2 className="formh2">basic information</h2>
 
-	                    
-	                      <label htmlFor="subject">Subject: </label>
 	                      <select className="form-control" name="unitName" id="subject" onChange={this.handleChange}>
-	                      	<option selected="selected">Select a Unit</option>
+	                      	<option selected="selected">select a subject</option>
 	                        <option value="JavaScript">JavaScript</option>
 	                        <option value="Math">Math</option> 
 	                        <option value="English">English</option>
 	                        <option value="Python">Python</option>
 	                        <option value="Biology">Biology</option>
 	                      </select>
+                          <input className="form-input" type="text" id="name" name="name" onChange={this.handleChange} placeholder="name"/>
 	                      <input className="form-input larger" type="text" id="description" name="description" onChange={this.handleChange} placeholder="description"/>
 	                      <input className="form-input" type="text" id="setbg" name="bg" onChange={this.handleChange} placeholder="background image url"/>
-	                      <button id="saveDeck" onClick={this.saveTopic}>Create Deck</button>
+	                      <button id="saveDeck" className="white-button" onClick={this.saveTopic}>Create Deck</button>
 	            </div>
 	        </div>    
 
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -34,78 +34,12 @@ class ConnectedDashboard extends React.Component {
             id: this.props.user.id
         })
 
-class ConnectedDashboard extends React.Component {
-  state = {
-    firstName: "",
-    id: "",
-    flashcards: []
-  };
+        let self = this;
 
-  handleDeleteButton = flashcardToRemove => {
-    this.setState(prevState => ({
-      flashcards: prevState.flashcards.filter(
-        flashcard => flashcardToRemove !== flashcard
-      )
-    }));
-    console.log("delete button clicked");
-  };
-
-  componentDidMount() {
-    this.setState({
-      firstName: this.props.user.firstName,
-      id: this.props.user.id
-    });
-
-    let self = this;
-    var data = {
-      user_id: this.props.user.id
-    };
-
-    fetch("/api/units", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      method: "POST",
-      body: JSON.stringify(data)
-    })
-      .then(function(response) {
-        if (response.status >= 400) {
-          throw new Error("Bad response from server");
+        var data = {
+            user_id: this.props.user.id
         }
-        console.log(response);
-        var data = JSON.stringify(response);
-        console.log(data);
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-        self.setState({
-          flashcards: data
-        });
-      })
-      .catch(err => {
-        console.log("caught it!", err);
-      });
-  }
 
-  render() {
-    return (
-      <div>
-        <Navbar firstName={this.state.firstName} />
-        <div className="container">
-          <Flashcards
-            flashcards={this.state.flashcards}
-            handleDeleteButton={this.handleDeleteButton}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-
-
-const Dashboard = connect(mapStateToProps)(ConnectedDashboard);
         fetch('/api/units', {
             headers : { 
                 'Content-Type': 'application/json',
@@ -114,7 +48,6 @@ const Dashboard = connect(mapStateToProps)(ConnectedDashboard);
             method: 'POST',
             body:JSON.stringify(data)
 
-           
         }).then(function(response) {
             if (response.status >= 400) {
                 throw new Error("Bad response from server");
@@ -151,5 +84,7 @@ const Dashboard = connect(mapStateToProps)(ConnectedDashboard);
         );
     }
 };
+
+const Dashboard = connect(mapStateToProps)(ConnectedDashboard);
 
 export default Dashboard;
