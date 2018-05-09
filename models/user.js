86fe82c697eb2c30635b6bc182449621d9d1a683
Kara Mui <kara.mui@dartmouth.edu>
2018-05-09 11:33:05 -0500

    converted to es6 and standardized formatting

--- models/user.js
+++ models/user.js
@@ -1,52 +1,50 @@
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
 
-module.exports = function(sequelize, DataTypes) {
-var User = sequelize.define("User", {
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [1,50]
-      }
-    },
+module.exports = (sequelize, DataTypes) => {
+    var User = sequelize.define("User", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [1,50]
+            }
+        },
 
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [1,50]
-      }
-    },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [1,50]
+            }
+        },
 
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [1,50]
-      }
-    },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [1,50]
+            }
+        },
 
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [1,100]
-      }
-    }
-  }, {
-    timestamps: false
-  });
-  
-  User.associate = function(models) {
-    User.hasMany(models.Unit, {
-        onDelete: "cascade"
-    });
-  };
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [1,100]
+            }
+        }}, {
+            timestamps: false
+        });
+      
+        User.associate = models => {
+            User.hasMany(models.Unit, {
+                onDelete: "cascade"
+            });
+        };
 
-  return User;
+    return User;
 };
-
