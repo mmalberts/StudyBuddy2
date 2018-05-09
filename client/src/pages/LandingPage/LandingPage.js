4b6e60168679716feae83fb04fe1224e783fa39d
Kara Mui <kara.mui@dartmouth.edu>
2018-05-08 20:37:25 -0500

    fixed formatting

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -3,13 +3,12 @@ import Header from "../../components/Header";
 import UserLogin from "../../components/UserLogin";
 
 export default class LandingPage extends React.Component {
-
   render() {
-    return (
-      <div className="background homepage">
-      <Header />
-      <UserLogin />
-      </div>
-    );
-  }
+    	return (
+      		<div className="background homepage">
+		      	<Header />
+		      	<UserLogin />
+		    </div>
+    	);
+  	}
 }
