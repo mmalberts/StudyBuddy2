c4e01aca981d3c01b0c2db9bd9e68a803c2efec4
Kara Mui <kara.mui@dartmouth.edu>
2018-05-08 19:58:57 -0500

    fixed first error

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -69,6 +69,46 @@ class ConnectedDashboard extends React.Component {
 
         }).catch(err => {
             console.log('caught it!', err);
+        });
+    }
+
+  componentDidMount() {
+    this.setState({
+      firstName: this.props.user.firstName,
+      id: this.props.user.id
+    });
+
+    let self = this;
+
+    var data = {
+      user_id: this.props.user.id
+    };
+
+    fetch("/api/units", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify(data)      
+      }).then(function(response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            console.log(response);
+            var data = JSON.stringify(response);
+            console.log(data);
+            return response.json();
+        }).then(function(data) {
+            console.log(data);
+            self.setState({
+                //flashcards: data,
+                filteredFlashcards: data
+            })
+
+
+        }).catch(err => {
+            console.log('caught it!', err);
         })
 
         // this.props.addUser({
