8d3a0527baeb17ef6907db648dd073b7ac2343f6
Kara Mui <kara.mui@dartmouth.edu>
2018-05-08 19:11:30 -0500

    cleaned up flashcard

--- client/src/components/Flashcard/flashcard.js
+++ client/src/components/Flashcard/flashcard.js
@@ -5,14 +5,13 @@ import classnames from 'classnames';
 const Flashcard = (props) => {
 
 	const cardBg = {
-		//backgroundImage: "url(http://i0.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg)"
 		backgroundImage: "url(" + props.flashcardBg + ")"
 	};
 
   return (
     <Link to={props.cardLink}>
     <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cardbox">
-      <div className={classnames("card", "card-default", props.flashcardBg)} style={cardBg}>
+      <div className={classnames("card", "card-default")} style={cardBg}>
         <button className="white-button card-title">{props.flashcardText}</button>
         <button 
          className="gradient-button delete" 
