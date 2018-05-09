21db17cce752f03655020953a352d76fcb62bb62
Kara Mui <kara.mui@dartmouth.edu>
2018-05-08 20:34:47 -0500

    removed addcard

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
+import AddUnit from "./pages/AddUnit";
 import Dashboard from "./pages/Dashboard";
-import SetPage from "./pages/SetPage";
-import QuestionPage from "./pages/QuestionPage";
-import Add from "./pages/Add";
 import DoesNotExist from "./pages/DoesNotExist";
+import LandingPage from "./pages/LandingPage";
+import QuestionPage from "./pages/QuestionPage";
+import SetPage from "./pages/SetPage";
 
 const App = () => (
   <BrowserRouter>
@@ -14,7 +14,7 @@ const App = () => (
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/setpage" component={SetPage} /> 
       <Route path="/questionpage" component={QuestionPage} />
-      <Route path="/add" component={Add} /> 
+      <Route path="/add" component={AddUnit} /> 
       <Route component={DoesNotExist} />    
    </Switch>
   </BrowserRouter>
