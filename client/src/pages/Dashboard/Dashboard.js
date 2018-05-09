30fc4492354891f6cf29b904cf0105029d958585
Kara Mui <kara.mui@dartmouth.edu>
2018-05-08 20:34:47 -0500

    removed addcard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import Filters from "../../components/Filters";
 import Flashcards from "../../components/Flashcards";
 import "./dashboard.css";
 import {connect} from "react-redux";
