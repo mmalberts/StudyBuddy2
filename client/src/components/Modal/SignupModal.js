f53cb92b685dde7b8bf6fe8e357804be25527a38
Kara Mui <kara.mui@dartmouth.edu>
2018-05-09 10:51:45 -0500

    cleaned userlogin

--- client/src/components/Modal/SignupModal.js
+++ client/src/components/Modal/SignupModal.js
@@ -4,27 +4,27 @@ import Modal from "react-modal";
 import "./SignupModal.css";
 
 const SignupModal = props => {
-  return (
-    <Modal
-      isOpen={props.confirmSignup}
-      contentLabel="Confirm Signup"
-      onRequestClose={props.handleClearModal}
-      className="modalpanel"
-      ariaHideApp={false}
-    >
-      {props.confirmSignup ? (
-        <div className="modalbox">
-          <h1>Thank you for signing up!</h1>
-          <Link to="/"><button onClick={props.handleClearModal} className="gradient-button modalbutton">get started!</button></Link>
-        </div>
-      ) : (
-        <div className="modalbox">
-          <h1>Sorry, user aleady exists!</h1>
-          <button onClick={props.handleClearModal} className="gradient-button modalbutton">Oh no!</button>
-        </div>
-      )}
-    </Modal>
-  );
+    return (
+        <Modal
+            isOpen={props.confirmSignup}
+            contentLabel="Confirm Signup"
+            onRequestClose={props.handleClearModal}
+            className="modalpanel"
+            ariaHideApp={false}
+        >
+            {props.confirmSignup ? (
+                <div className="modalbox">
+                    <h1>Thank you for signing up!</h1>
+                    <Link to="/"><button onClick={props.handleClearModal} className="gradient-button modalbutton">get started!</button></Link>
+                </div>
+            ) : (
+                <div className="modalbox">
+                    <h1>Sorry, user aleady exists!</h1>
+                    <button onClick={props.handleClearModal} className="gradient-button modalbutton">Oh no!</button>
+                </div>
+            )};
+        </Modal>
+    );
 };
 
 export default SignupModal;
