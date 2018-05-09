a877d50f605b281e9d5b50447cda9473501b2370
Kara Mui <kara.mui@dartmouth.edu>
2018-05-08 19:06:47 -0500

    unit backgrounds working

--- client/src/components/Flashcard/flashcard.js
+++ client/src/components/Flashcard/flashcard.js
@@ -4,10 +4,15 @@ import classnames from 'classnames';
 
 const Flashcard = (props) => {
 
-  return(
+	const cardBg = {
+		//backgroundImage: "url(http://i0.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg)"
+		backgroundImage: "url(" + props.flashcardBg + ")"
+	};
+
+  return (
     <Link to={props.cardLink}>
     <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cardbox">
-      <div className={classnames("card", "card-default", props.flashcardBg)}>
+      <div className={classnames("card", "card-default", props.flashcardBg)} style={cardBg}>
         <button className="white-button card-title">{props.flashcardText}</button>
         <button 
          className="gradient-button delete" 
