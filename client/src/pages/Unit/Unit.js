ec7659bf785f31b38c88f3018e6f0f892205ff63
Chetan Audipudy <chetan.audipudy@gmail.com>
2018-05-10 19:59:21 -0500

    working question form, needs styling

--- client/src/pages/Unit/Unit.js
+++ client/src/pages/Unit/Unit.js
@@ -5,42 +5,52 @@ import Navbar from "../../components/Navbar";
 import "./Unit.css";
 
 const mapStateToProps = state => {
-	return { user: state[0] };
-} 
+  return { user: state[0] };
+};
 
 class ConnectedUnit extends React.Component {
-	state = {
-	    title: "Vocabulary",
-	    description: "A simple vocab test to quiz your knowledge.",
-	    user: "Amanda Ramirez",
-	    link: "./questionpage"
-  	};
+  state = {
+    title: "Vocabulary",
+    description: "A simple vocab test to quiz your knowledge.",
+    user: "Amanda Ramirez",
+    link: "./questionpage"
+  };
 
-    render() {
-        return (
-            <div className="background example-page">
-	            <Navbar firstName = {this.props.user.firstName}/>
-                
-                <div className="row">
-			        <div className="about">
-			          	<div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-				            <button className="gradient-button set-title">{this.state.title}</button>
-				            <h3 className="byline">by {this.state.user}</h3>
-			          	</div>
+  render() {
+    return (
+      <div className="background example-page">
+        <Navbar firstName={this.props.user.firstName} />
 
-				        <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-9">
-				            <div className="description">
-				              	{this.state.description}
-				              	<Link to={this.state.link}><button className="gradient-button split">continue studying</button></Link>
-				              	<Link to="/dashboard"><button className="gradient-button split">edit flashcards</button></Link>
-				            </div>
-				        </div>
-				    </div>
-				</div>
+        <div className="row">
+          <div className="about">
+            <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+              <button className="gradient-button set-title">
+                {this.state.title}
+              </button>
+              <h3 className="byline">by {this.state.user}</h3>
             </div>
-        );
-    };
-};
+
+            <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-9">
+              <div className="description">
+                {this.state.description}
+                <Link to={this.state.link}>
+                  <button className="gradient-button split">
+                    continue studying
+                  </button>
+                </Link>
+                <Link to="/AddQuestion">
+                  <button className="gradient-button split">
+                    edit flashcards
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 const Unit = connect(mapStateToProps)(ConnectedUnit);
 
