6dbb13c793e07e26b27d0e195fd997f0eb5d9bf4
Christopher Banaszak <ludachris@dhcp-10-105-83-54.wireless.northwestern.private>
2018-05-10 20:04:32 -0500

    changed card model to fit component

--- models/card.js
+++ models/card.js
@@ -1,19 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
 	var Card = sequelize.define("Card", {
-		problem: {
+		question: {
 			type: DataTypes.STRING,
 			allowNull: false
 		},
-		solution: {
+		correctAnswer: {
 			type: DataTypes.STRING,
 			allowNull: false
 		},
-		incorrect1: {
+		answer1: {
 			type: DataTypes.STRING,
 			allowNull: false
 		},
-		incorrect2: {
+		answer2: {
 			type: DataTypes.STRING,
+			allowNull: false
+		},
+		answer3: {
+		    type: DataTypes.STRING,
 			allowNull: false
 		},
 		gotItRight: {
