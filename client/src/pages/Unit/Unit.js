dbdd8ee3a6c0c62e0f3d53eb0fa66a5041036f77
Christopher Banaszak <ludachris@dhcp-10-105-83-54.wireless.northwestern.private>
2018-05-10 19:12:58 -0500

    unit page up and running with unit information

--- client/src/pages/Unit/Unit.js
+++ client/src/pages/Unit/Unit.js
@@ -9,17 +9,48 @@ const mapStateToProps = state => {
 };
 
 class ConnectedUnit extends React.Component {
-  state = {
-    title: "Vocabulary",
-    description: "A simple vocab test to quiz your knowledge.",
-    user: "Amanda Ramirez",
-    link: "./questionpage"
-  };
+	state = {
+	    title: "",
+	    description: "",
+	    user: this.props.user.firstName + ' ' + this.props.user.lastName,
+	    link: "./questionpage"
+  	};
 
-  render() {
-    return (
-      <div className="background example-page">
-        <Navbar firstName={this.props.user.firstName} />
+  	componentDidMount(){
+
+		let self = this;
+	    var unitId = {id:Number(this.props.match.params.id)};
+
+	    fetch("/api/units/unit", {
+	      headers: {
+	        "Content-Type": "application/json",
+	        Accept: "application/json"
+	      },
+	      method: "POST",
+	      body: JSON.stringify(unitId)  
+
+	      }).then(function(response) {
+	            if (response.status >= 400) {
+	                throw new Error("Bad response from server");
+	            }
+	            console.log(response);
+	            //var data = JSON.stringify(response);
+	            //console.log(data);
+	            return response.json();
+	        }).then(function(data) {
+	            self.setState({
+	                title: data[0].unitName,
+	                description: data[0].description
+	            })
+	        }).catch(err => {
+	            console.log('caught it!', err);
+	        })
+  	}
+
+    render() {
+        return (
+            <div className="background example-page">
+	            <Navbar firstName = {this.props.user.firstName}/>
 
         <div className="row">
           <div className="about">
