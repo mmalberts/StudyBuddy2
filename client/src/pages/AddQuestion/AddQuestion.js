b38f7cf0ed5a11ddbf403c223cee70335209268e
Kara Mui <kara.mui@dartmouth.edu>
2018-05-12 11:33:29 -0500

    styled question form

--- client/src/pages/AddQuestion/AddQuestion.js
+++ client/src/pages/AddQuestion/AddQuestion.js
@@ -1,46 +1,35 @@
 import React, { Component } from "react";
 import CreateQuestion from "../../components/Question/CreateQuestion";
 import Navbar from "../../components/Navbar/Navbar";
+import "./AddQuestion.css";
 
 export default class AddQuestion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userQuestions: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            userQuestions: ["q 1", "q 2", "q 3"]
+        };
     };
-  }
 
-  componentDidMount(){
-        let self = this;
-        var data = {
-          UserId: this.props.match.params.unitId
-        }
-        var questionsArr = [];
+    render() {
+        return (
+            <div>
+                <Navbar />
 
-        fetch("/api/cards/" + this.props.match.params.unitId, {
-            method: "POST",
-            body: data
-        }).then(response => {
-            return response.json();         
-        }).then(responseJson => {
-          console.log(responseJson);
-          self.setState({
-            userQuestions: responseJson
-          })
-        }).catch(err => {
-            console.log("Error: ", err);
-        });
-  }
-
-  render() {
-    return (
-      <div>
-         <Navbar /> 
-        {this.state.userQuestions && (
-          <ul>{this.state.userQuestions.map(q => <li>{q.question}</li>)}</ul>
-        )}
-        <CreateQuestion unitId={this.props.match.params.unitId} />
-      </div>
-    );
-  }
-}
+                <div className="container">
+                    <div className="qholder">
+                        {this.state.userQuestions && (
+                            <ul>
+                                {this.state.userQuestions.map(q => 
+                                    <li>{q}</li>
+                                )}
+                            </ul>
+                        )}
+                    
+                        <CreateQuestion unitId={this.props.match.params.unitId} />
+                    </div>
+                </div>
+            </div>
+        );
+    };
+};
