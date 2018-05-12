3501bdd662e62193f2e76dd073b746b864b6f648
Kara Mui <kara.mui@dartmouth.edu>
2018-05-12 11:33:29 -0500

    styled question form

--- client/src/pages/AddQuestion/AddQuestion.js
+++ client/src/pages/AddQuestion/AddQuestion.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import CreateQuestion from "../../components/Question/CreateQuestion";
 import Navbar from "../../components/Navbar/Navbar";
+import "./AddQuestion.css";
 
 export default class AddQuestion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userQuestions: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            userQuestions: ["q 1", "q 2", "q 3"]
+        };
     };
-  }
 
   componentDidMount(){
         let self = this;
@@ -32,15 +33,26 @@ export default class AddQuestion extends Component {
         });
   }
 
-  render() {
-    return (
-      <div>
-         <Navbar /> 
-        {this.state.userQuestions && (
-          <ul>{this.state.userQuestions.map(q => <li>{q.question}</li>)}</ul>
-        )}
-        <CreateQuestion unitId={this.props.match.params.unitId} />
-      </div>
-    );
-  }
-}
+    render() {
+        return (
+            <div>
+                <Navbar />
+
+                <div className="container">
+                    <div className="qholder">
+                        {this.state.userQuestions && (
+                            <ul>
+                                {this.state.userQuestions.map(q => 
+                                    <li>{q}</li>
+                                )}
+                            </ul>
+                        )}
+                    
+                        <CreateQuestion unitId={this.props.match.params.unitId} />
+                    </div>
+                </div>
+            </div>
+        );
+    };
+};
+
