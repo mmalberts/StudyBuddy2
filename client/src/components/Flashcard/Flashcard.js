a17d711f7f3d23caccb34a1c3893c644eafc53c4
mmalberts <maggiemalberts@gmail.com>
2018-05-12 13:02:50 -0500

    delete buttons fixed

--- client/src/components/Flashcard/Flashcard.js
+++ client/src/components/Flashcard/Flashcard.js
@@ -8,14 +8,14 @@ const Flashcard = props => {
 	};
 
     return (
-        <Link to={props.cardLink}>
-            <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cardbox">
-                <div className={classnames("card", "card-default")} style={cardBg}>
+        <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cardbox">
+            <div className={classnames("card", "card-default")} style={cardBg}>
+                <Link to={props.cardLink}>
                     <button className="white-button card-title">{props.flashcardText}</button>
-                    <button className="gradient-button delete" id={props.id} onClick={props.handleDeleteButton}>x</button>
-                </div>
+                </Link>
+                <button className="gradient-button delete" id={props.id} onClick={props.handleDeleteButton}>x</button>
             </div>
-        </Link>
+        </div>
     );
 };
 
