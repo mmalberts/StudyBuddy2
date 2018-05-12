c9e3a7f0697491b386f30bf9124a60b68b625a8b
Christopher Banaszak <ludachris@dhcp-10-105-83-54.wireless.northwestern.private>
2018-05-12 11:36:28 -0500

    added fetch to get unit questions

--- client/src/pages/AddQuestion/AddQuestion.js
+++ client/src/pages/AddQuestion/AddQuestion.js
@@ -6,16 +6,38 @@ export default class AddQuestion extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userQuestions: ["q 1", "q 2", "q 3"]
+      userQuestions: []
     };
+  }
+
+  componentDidMount(){
+        let self = this;
+        var data = {
+          UserId: this.props.match.params.unitId
+        }
+        var questionsArr = [];
+
+        fetch("/api/cards/" + this.props.match.params.unitId, {
+            method: "POST",
+            body: data
+        }).then(response => {
+            return response.json();         
+        }).then(responseJson => {
+          console.log(responseJson);
+          self.setState({
+            userQuestions: responseJson
+          })
+        }).catch(err => {
+            console.log("Error: ", err);
+        });
   }
 
   render() {
     return (
       <div>
-        <Navbar />
+         <Navbar /> 
         {this.state.userQuestions && (
-          <ul>{this.state.userQuestions.map(q => <li key={q}>{q}</li>)}</ul>
+          <ul>{this.state.userQuestions.map(q => <li>{q.question}</li>)}</ul>
         )}
         <CreateQuestion unitId={this.props.match.params.unitId} />
       </div>
