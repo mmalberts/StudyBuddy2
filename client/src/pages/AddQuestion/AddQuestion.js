ddb52215c9199d3698c044ac0838d22152c5ba77
Kara Mui <kara.mui@dartmouth.edu>
2018-05-15 12:38:14 -0500

    unified formatting

--- client/src/pages/AddQuestion/AddQuestion.js
+++ client/src/pages/AddQuestion/AddQuestion.js
@@ -1,77 +1,72 @@
 import React, { Component } from "react";
-import CreateQuestion from "../../components/Question/CreateQuestion";
-import Navbar from "../../components/Navbar/Navbar";
-import "./AddQuestion.css";
 import { connect } from "react-redux";
+import CreateQuestion from "../../components/Question/CreateQuestion";
+import Navbar from "../../components/Navbar";
+import "./AddQuestion.css";
 
 const mapStateToProps = state => {
-  return { user: state[0] };
+    return { user: state[0] };
 };
 
 class AddQuestion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userQuestions: []
+    constructor(props) {
+        super(props);
+        this.state = { userQuestions: [] };
     };
-  }
-  componentDidMount(){
-      let self = this;
-      var data = {
-        UserId: this.props.match.params.unitId
-      }
-      var questionsArr = [];
 
-       fetch("/api/cards/" + this.props.match.params.unitId, {
-          method: "POST",
-          body: data
-      })
-       .then(response => {
-          return response.json();         
-      })
-       .then(responseJson => {
-        console.log(responseJson);
-        self.setState({
-          userQuestions: responseJson
-        })
-       })
-       .catch(err => {
-          console.log("Error: ", err);
-      });
-  }
+    componentDidMount() {
+        let self = this;
 
-  handleDeleteQuestion = e => {
-    var url = "/api/cards/delete/" + e.target.id;
-    fetch(url, {
-      method: "DELETE"
-    }).then(response => response.json);
-    window.location.reload();
-  };
+        var data = {
+            UserId: this.props.match.params.unitId
+        };
 
-  render() {
-    return (
-      <div>
-        <Navbar firstName={this.props.user.firstName} />
+        fetch("/api/cards/" + this.props.match.params.unitId, {
+            method: "POST",
+            body: data
+        }).then(response => {
+            return response.json();
+        }).then(responseJson => {
+            self.setState({ userQuestions: responseJson });
+        }).catch(err => {
+            console.log("Error: " + err);
+        });
+    };
 
-        <div className="container">
-          <div className="qholder">
-            {this.state.userQuestions && (
-              <div>
-                {this.state.userQuestions.map(q => (
-                  <p>
-                    <button id={q.id} onClick={this.handleDeleteQuestion} className="white-delete">x</button>
-                    {q.question}
-                  </p>
-                ))}
-              </div>
-            )}
-            
-            <CreateQuestion unitId={this.props.match.params.unitId} />
-          </div>
-        </div>
-      </div>
-    );
-  };
+    handleDeleteQuestion = e => {
+        var url = "/api/cards/delete/" + e.target.id;
+        
+        fetch(url, {
+            method: "DELETE"
+        }).then(response => response.json);
+    
+        window.location.reload();
+    };
+
+    render() {
+        return (
+            <div>
+                <Navbar firstName={this.props.user.firstName} />
+
+                <div className="container">
+                    <div className="qholder">
+                        { this.state.userQuestions && (
+                            <div>
+                                { this.state.userQuestions.map(q => 
+                                    <p>
+                                        <button id={q.id} onClick={this.handleDeleteQuestion} className="white-delete">x</button>
+                                        {q.question}
+                                    </p>
+                                )}
+                            </div>
+                        )}
+
+                        <CreateQuestion unitId={this.props.match.params.unitId} />
+                    </div>
+                </div>
+            </div>
+        );
+    };
 };
 
 export default connect(mapStateToProps)(AddQuestion);
