e76e49ae5acea61adf7fc345b7e1128e48b5d4a9
Kara Mui <kara.mui@dartmouth.edu>
2018-05-15 11:52:44 -0500

    cleaned formatting

--- client/src/pages/Unit/Unit.js
+++ client/src/pages/Unit/Unit.js
@@ -5,71 +5,66 @@ import Navbar from "../../components/Navbar";
 import "./Unit.css";
 
 const mapStateToProps = state => {
-  return { user: state[0] };
+    return { user: state[0] };
 };
 
 class ConnectedUnit extends React.Component {
-  state = {
-    title: "",
-    description: "",
-    user: this.props.user.firstName + " " + this.props.user.lastName,
-    bg: "",
-    link: "./questionpage"
-  };
+    state = {
+        title: "",
+        description: "",
+        user: this.props.user.firstName + " " + this.props.user.lastName,
+        bg: "",
+        link: "./question"
+    };
 
-  componentDidMount() {
-    let self = this;
-    var unitId = { id: Number(this.props.match.params.id) };
+    componentDidMount() {
+        let self = this;
+        var unitId = { id: Number(this.props.match.params.id) };
 
-    fetch("/api/units/unit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify(unitId)
-    })
-      .then(response => {
-        if (response.status >= 400) {
-          throw new Error("Bad response from server.");
-        }
-        return response.json();
-      })
-      .then(data => {
-        self.setState({
-          title: data[0].unitName,
-          description: data[0].description,
-          bg: data[0].bg
+        fetch("/api/units/unit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(unitId)
+        }).then(response => {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server.");
+            }
+            return response.json();
+        }).then(data => {
+            self.setState({
+                title: data[0].unitName,
+                description: data[0].description,
+                bg: data[0].bg
+            });
+        }).catch(err => {
+            console.log("Error: " + err);
         });
-      })
-      .catch(err => {
-        console.log("Error: " + err);
-      });
-  }
+    };
 
-  render() {
-    return (
-      <div
-        className="background unitpage"
-        style={{ backgroundImage: "url(" + this.state.bg + ")" }}
-      >
-        <Navbar firstName={this.props.user.firstName} />
+    render() {
+        return (
+            <div className="background unitpage" style={{ backgroundImage: "url(" + this.state.bg + ")" }}>
+                <Navbar firstName={this.props.user.firstName} />
 
-        <div className="row">
-          <div className="about">
-            <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-              <button className="gradient-button set-title">
-                {this.state.title}
-              </button>
-              <h3 className="byline">by {this.state.user}</h3>
-            </div>
+                <div className="row">
+                    <div className="about">
+                        <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                            <button className="gradient-button set-title">{this.state.title}</button>
+                            <h3 className="byline">by {this.state.user}</h3>
+                        </div>
+
+
                         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-9">
                             <div className="description">
                                 {this.state.description}
-                                <Link to={"/questionpage/" + this.props.match.params.id}>
+                                <Link to={"/question/" + this.props.match.params.id}>
                                     <button className="gradient-button split">continue studying</button>
                                 </Link>
-                                <Link to={"/AddQuestion/" + this.props.match.params.id}>
+                            
+                                <Link to={"/addquestion/" + this.props.match.params.id}>
                                     <button className="gradient-button split">edit flashcards</button>
                                 </Link>
                             </div>
@@ -77,9 +72,9 @@ class ConnectedUnit extends React.Component {
                     </div>
                 </div>
             </div>
-    );
-  }
-}
+        );
+    };
+};
 
 const Unit = connect(mapStateToProps)(ConnectedUnit);
 
