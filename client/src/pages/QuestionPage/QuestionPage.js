0d10712f9b710d39f307e5e583a9a91b0c25fda5
Kara Mui <kara.mui@dartmouth.edu>
2018-05-15 19:09:30 -0500

    style fixes

--- client/src/pages/QuestionPage/QuestionPage.js
+++ client/src/pages/QuestionPage/QuestionPage.js
@@ -35,16 +35,19 @@ class QuestionPage extends React.Component {
         fetch("/api/cards/" + this.props.match.params.unitId, {
             method: "POST",
             body: data
-        }).then(response => {
+        })
+        .then(response => {
             return response.json();
-        }).then(responseJson => {
+        })
+        .then(responseJson => {
             this.setState(() => ({
                 userQuestions: responseJson,
                 title: responseJson[0].Unit.unitName,
                 currentQ: responseJson[0],
                 totalQuestions: responseJson.length
             }));
-        }).catch(err => {
+        })
+        .catch(err => {
             console.log("Error: " + err);
         });
     };
@@ -62,30 +65,37 @@ class QuestionPage extends React.Component {
                 num: this.state.num + 1
             }));
         }
-    };
+    }
 
     handleAnsClick = e => {
         let correctAnswer = e.correctAnswer;
         let ans = e.chosenAns;
-        let percent = this.state.progress + (Math.round(1 / this.state.totalQuestions * 100));
+        let percent = 
+            this.state.progress + (Math.round(1 / this.state.totalQuestions * 100));
 
         if (this.state.currentQuestion === this.state.totalQuestions - 1) {
             percent = 100;
         }
     
         if (correctAnswer === ans) {
-            this.setState(() => ({
-                ansStatus: true,
-                currentQuestion: this.state.currentQuestion + 1,
-                progress: percent,
-                score: this.state.score + 1
-            }), this.resetState);
+            this.setState(
+                () => ({
+                    ansStatus: true,
+                    currentQuestion: this.state.currentQuestion + 1,
+                    progress: percent,
+                    score: this.state.score + 1
+                }), 
+                this.resetState
+            );
         } else {
-            this.setState(() => ({
-                ansStatus: false,
-                currentQuestion: this.state.currentQuestion + 1,
-                progress: percent
-            }), this.resetState);
+            this.setState(
+                () => ({
+                    ansStatus: false,
+                    currentQuestion: this.state.currentQuestion + 1,
+                    progress: percent
+                }), 
+                this.resetState
+            );
         }
     };
 
@@ -95,27 +105,31 @@ class QuestionPage extends React.Component {
                 <Navbar firstName={this.props.user.firstName} />
 
                 <div className="row info">
-                    <button className="white-button mini-title">{this.state.title}</button>
+                    <button className="white-button mini-title">
+                        {this.state.title}
+                    </button>
                     <h3 className="mini-byline">by {this.state.user}</h3>
-                    <button className="white-button mini-title score">{this.state.score} / {this.state.totalQuestions}</button>
+                    <button className="white-button mini-title score">
+                        {this.state.score} / {this.state.totalQuestions}
+                    </button>
                 </div>
 
                 <div className="container text-center ml-auto mr-auto">
-                    <div className="row content">
-                        { this.state.num < this.state.userQuestions.length ? (
-                            <Question
-                                key={this.state.currentQ.id}
-                                question={this.state.currentQ.question}
-                                answer1={this.state.currentQ.answer1}
-                                answer2={this.state.currentQ.answer2}
-                                answer3={this.state.currentQ.answer3}
-                                correctAnswer={this.state.currentQ.correctAnswer}
-                                handleAnsClick={this.handleAnsClick}
-                            />
-                        ) : (
-                            <EndOfQuiz score={this.state.score} />
-                        )}
-                    </div>
+                  <div className="row content">
+                    {this.state.num < this.state.userQuestions.length ? (
+                      <Question
+                        key={this.state.currentQ.id}
+                        question={this.state.currentQ.question}
+                        answer1={this.state.currentQ.answer1}
+                        answer2={this.state.currentQ.answer2}
+                        answer3={this.state.currentQ.answer3}
+                        correctAnswer={this.state.currentQ.correctAnswer}
+                        handleAnsClick={this.handleAnsClick}
+                      />
+                    ) : (
+                      <EndOfQuiz score={this.state.score} total={this.state.totalQuestions} />
+                    )}
+                  </div>
                 </div>
 
                 <div className="progress">
